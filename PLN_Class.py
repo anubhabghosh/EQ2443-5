41092b8cc6b0124b6b7182acd07c6fd6003333b7
Anubhab Ghosh <findghosh@gmail.com>
2019-11-13 12:35:30 +0100

    Made some fixes in the ADMM Function

--- PLN_Class.py
+++ PLN_Class.py
@@ -9,7 +9,7 @@ class PLN():
         self.n_l = layer_no
         self.V_Q = np.concatenate((np.eye(Q), -1*np.eye(Q)), axis=0)
         self.R_l = self.initialise_random_matrix(n_hidden - 2*Q, X.shape[0])
-        self.O_l = self.initialise_random_matrix(n_hidden, Q)# Needs to be optimized by SGD/ADMM
+        self.O_l = self.initialise_random_matrix(Q, n_hidden)# Needs to be optimized by SGD/ADMM
         self.Y_l = np.zeros((n_hidden,1))
         #if W_ls.all() == None:
         #    pass
@@ -19,9 +19,11 @@ class PLN():
 
     # Computes element wise activation 
     def activation_function(self, Z):
-        return np.where(Z >=0, Z, 0)
+        return np.where(Z > 0, Z, 0)
 
     def initialise_random_matrix(self,M,N):
+        #A =  np.random.uniform(low = -1, high = 1, size = (M,N)) 
+        #return A - np.mean(A)
         return np.random.normal(0, 1, size=(M, N))
         
     def normalization(self, A):
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from Admm import optimize_admm
 from LoadDataFromMat import importData
 import numpy as np
 
-
 # Import the dataset and calculate related parameters
 
 def importDummyExample():
@@ -32,66 +31,72 @@ def importDummyExample():
 def compute_Wls(X,T,lam):
 
     # the X are in n*p form, n sample, each sample has p dims. T is n*Q matrix, each sample is a row vector
-    #print(X.shape)
-    #print(T.shape)
     inv_matrix = np.linalg.inv(np.dot(X, X.T)+lam*np.eye(X.shape[0]))
     W_ls = np.dot(np.dot(T, X.T), inv_matrix).astype(np.float32)
-    #W_ls = np.dot(T, inv_matrix).dot(X.T)
-    #print(W_ls.shape)
-    #Q = T.shape[0]
     return W_ls
 
 def compute_ol(Y,T,mu, max_iterations):
 
+    # Computes the Output matrix by calling the ADMM Algorithm function with given parameters
     ol = optimize_admm(T, Y, mu, max_iterations)
     return ol
 
 def compute_accuracy(predicted_lbl, true_lbl):
 
+    # Computes a Classification Accuracy between true label
     acc = 100.*np.mean(np.argmax(predicted_lbl,axis=0)==np.argmax(true_lbl,axis=0))
     return acc
 
 def compute_test_outputs(PLN_object_array, W_ls, num_layers, X_test):
 
+    # Computes the network output for the first layer
     PLN_1 = PLN_object_array[0]
     W_initial_top = np.dot(np.dot(PLN_1.V_Q, W_ls), X_test)
     W_initial_bottom = PLN_1.normalization(np.dot(PLN_1.R_l, X_test))
     Z = np.concatenate((W_initial_top, W_initial_bottom), axis=0)
     y = PLN_1.activation_function(Z)
 
-    for i in range(1, num_layers-1):
+    # Computes the network output for each layer after the first layer
+    for i in range(1, num_layers):
         PLN_l = PLN_object_array[i]
         W_top = np.dot(np.dot(PLN_l.V_Q, PLN_object_array[i-1].O_l), y)
         W_bottom = PLN_l.normalization(np.dot(PLN_l.R_l, y))
         Z = np.concatenate((W_top, W_bottom), axis=0)
         y = PLN_l.activation_function(Z)
 
+    # Returns network output for the last layer
     return np.dot(PLN_object_array[num_layers - 1].O_l, y)
 
-# This function computes the Normalized Mean Error (NME) in dB scale
 def compute_NME(predicted_lbl, actual_lbl):
 
-    num = np.mean(np.linalg.norm((predicted_lbl-actual_lbl), axis=0)**2)
-    den = np.mean(np.linalg.norm((actual_lbl), axis=0)**2)
-    NME = 10 * np.log10(num/den)
+    # This function computes the Normalized Mean Error (NME) in dB scale
+
+    num = np.linalg.norm(actual_lbl - predicted_lbl, ord='fro') # Frobenius Norm of the difference between Predicted and True Label
+    den = np.linalg.norm(actual_lbl, ord='fro') # Frobenius Norm of the True Label
+    NME = 20 * np.log10(num / den)
     return NME
 
 def main():
 
-    #X, T = importDummyExample()
+    ##########################################################################################
+    # Dataset related parameters
     dataset_path = "../Datasets/" # Specify the dataset path in the Local without the name
     dataset_name = "Vowel" # Specify the Dataset name without extension (implicitly .mat extension is assumed)
     X_train, Y_train, X_test, Y_test, Q = importData(dataset_path, dataset_name) # Imports the data with the no. of output classes
-    mu = 1e5 # For the given dataset
-    max_iterations = 100 # For the ADMM Algorithm
-    #X_train, Y_train, X_test, Y_test, Q = importData(dataset_path)
-
-    lambda_ls = 1e6 # Given regularization parameter as used in the paper
-    Wls = compute_Wls(X_train, Y_train, lambda_ls)
-    print(Q)
-
-    # Compute Train and Test accuracy
     
+    mu = 1e3 # For the given dataset
+    lambda_ls = 1e2 # Given regularization parameter as used in the paper for the used Dataset
+    ##########################################################################################
+
+    ##########################################################################################
+    # Parameters related to ADMM optimization
+    max_iterations = 100 # For the ADMM Algorithm
+    ##########################################################################################
+
+    ##########################################################################################
+    # Compute Train and Test accuracy for a Regularized Least Squares Algorithm
+    ##########################################################################################
+    Wls = compute_Wls(X_train, Y_train, lambda_ls) 
     predict_train = np.dot(Wls, X_train)
     predict_test = np.dot(Wls, X_test)
     acc_train = compute_accuracy(predict_train, Y_train)
@@ -99,10 +104,14 @@ def main():
     nme_test = compute_NME(predict_test, Y_test)
     print("Train and test accuracies are: {} and {}".format(acc_train, acc_test))
     print("NME Test:{}".format(nme_test))
+
+    ##########################################################################################
     # Creating a list of PLN Objects
+    ##########################################################################################
     
-    PLN_objects = []
-    no_layers = 20 # Total number of layers to be useds
+    PLN_objects = [] # The network is to be stored as a list of objects, with each object 
+                     # representing a network layer
+    no_layers = 20 # Total number of layers to be used
 
     # Creating a 1 layer Network
     
@@ -126,19 +135,26 @@ def main():
     pln_l1.Y_l = pln_l1.activation_function(pln_l1_Z_l) 
 
     # Computing the Output Matrix by using 100 iterations of ADMM
+    print("ADMM for Layer No:{}".format(1))
     pln_l1.O_l = compute_ol(pln_l1.Y_l, Y_train, mu, max_iterations)
 
     # Appending the creating object for every layer to a list, which constitutes the 'network' 
     PLN_objects.append(pln_l1)
-    predicted_lbl = compute_test_outputs(PLN_objects, Wls, 1, X_test) # Predicts the test performance of the networks
-    print("Test Accuracy:{}".format(compute_accuracy(predicted_lbl, Y_test)))
-    print("Test NME:{}".format(compute_NME(predicted_lbl, Y_test)))
+    
+    predicted_lbl_test = compute_test_outputs(PLN_objects, Wls, 1, X_test)
+    predicted_lbl_train = compute_test_outputs(PLN_objects, Wls, 1, X_train)
+    print("Training Accuracy:{}".format(compute_accuracy(predicted_lbl_train, Y_train)))
+    print("Test Accuracy:{}".format(compute_accuracy(predicted_lbl_test, Y_test)))
+    print("Traning NME:{}".format(compute_NME(predicted_lbl_train, Y_train)))
+    print("Test NME:{}".format(compute_NME(predicted_lbl_test, Y_test)))
+
     # ADMM Training for all the remaining layers using the training Data
     # Also involves subsequent testing on the test data
     
     for i in range(1, no_layers):
 
-        print("ADMM for Layer No:{}".format(i))
+        print("************** ADMM for Layer No:{} **************".format(i+1))
+
         X = PLN_objects[i-1].Y_l # Input is the Output g(WX) for the previous layer
         num_node = 2*Q + 1000 # No. of nodes fixed for every layer
         pln = PLN(Q, X, i, num_node, W_ls=None) # Creating the PLN Object for the new layer
@@ -161,13 +177,16 @@ def main():
         # Add the new layer to the 'Network' list
         PLN_objects.append(pln)
 
-        # Compute test accuracy for new appended networks
-        predicted_lbl = compute_test_outputs(PLN_objects, Wls, i, X_test)
-        print("Test Accuracy:{}".format(compute_accuracy(predicted_lbl, Y_test)))
-        print("Test NME:{}".format(compute_NME(predicted_lbl, Y_test)))
+        # Compute training, test accuracy, NME for new appended networks
+        predicted_lbl_test = compute_test_outputs(PLN_objects, Wls, i+1, X_test)
+        predicted_lbl_train = compute_test_outputs(PLN_objects, Wls, i+1, X_train)
+        print("Training Accuracy:{}\n".format(compute_accuracy(predicted_lbl_train, Y_train)))
+        print("Test Accuracy:{}\n".format(compute_accuracy(predicted_lbl_test, Y_test)))
+        print("Traning NME:{}\n".format(compute_NME(predicted_lbl_train, Y_train)))
+        print("Test NME:{}\n".format(compute_NME(predicted_lbl_test, Y_test)))
+        
     #predicted_lbl = compute_test_outputs(PLN_objects, Wls, no_layers, X_test)
     #print("Test Accuracy:{}".format(compute_accuracy(predicted_lbl, Y_test)))
-    
     return None
 
 if __name__ == "__main__":
